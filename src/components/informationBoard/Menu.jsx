e5a1c84ca1337e3e7e01dcaab5093599e2b39eff
iagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2025-06-11 17:55:23 -0300

    feat: created navigation between barber pages

--- src/components/informationBoard/Menu.jsx
+++ src/components/informationBoard/Menu.jsx
@@ -1,22 +1,52 @@
 import styled from "styled-components";
+import { useRouter } from "next/navigation";
 
 import { CalendarIcon } from "@phosphor-icons/react/dist/ssr";
 import { ScissorsIcon } from "@phosphor-icons/react/dist/ssr";
 import { ChartLineIcon } from "@phosphor-icons/react/dist/ssr";
 
 const Container = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    width: 100%;
-`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+`;
+
+const StyledScissorsIcon = styled(ScissorsIcon)`
+  cursor: pointer;
+`;
+
+const StyledCalendarIcon = styled(CalendarIcon)`
+  cursor: pointer;
+`;
+
+const StyledChartLineIcon = styled(ChartLineIcon)`
+  cursor: pointer;
+`;
 
 export default function Menu({ page }) {
-    return(
-        <Container>
-            <ScissorsIcon size={50} color={page === "Serviços" ? "#FF7000" : "#949494"} weight="fill" />
-            <CalendarIcon size={50} color={page === "Agenda" ? "#FF7000" : "#949494"} weight="fill"  />
-            <ChartLineIcon size={50} color={page === "Relatórios" ? "#FF7000" : "#949494"} weight="fill"  />
-        </Container>
-    )
+  const router = useRouter();
+
+  return (
+    <Container>
+      <StyledScissorsIcon
+        size={50}
+        color={page === "Serviços" ? "#FF7000" : "#949494"}
+        weight="fill"
+        onClick={() => router.push("/servicos")}
+      />
+      <StyledCalendarIcon
+        size={50}
+        color={page === "Agenda" ? "#FF7000" : "#949494"}
+        weight="fill"
+        onClick={() => router.push("/agenda")}
+      />
+      <StyledChartLineIcon
+        size={50}
+        color={page === "Relatórios" ? "#FF7000" : "#949494"}
+        weight="fill"
+        onClick={() => router.push("/relatorio")}
+      />
+    </Container>
+  );
 }
